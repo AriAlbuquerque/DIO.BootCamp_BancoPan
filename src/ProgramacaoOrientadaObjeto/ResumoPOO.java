67847dde050edbb889bcfabd26dc2d98c5131db5
Ariane Albuquerque <arianeoalbuquerques@gmail.com>
2023-03-27 22:02:07 -0300

    Atualização uso de APACHE MAVEN

--- src/ProgramacaoOrientadaObjeto/ResumoPOO.java
+++ src/ProgramacaoOrientadaObjeto/ResumoPOO.java
@@ -5,18 +5,25 @@ public class ResumoPOO {
 
     /*  ***Programação Orientada á Objeto***
 
-    *** OBJETO---> É o que executa faz tudo acontecer, deve estar em uma classe estática, é a representação de um conceito/entidade do mundo real, que pode ser física ou conceitual;
+    PARADIGMA DE PROGRAMAÇÃO ORIENTADA A OBJETO: Modelo de desenvolvimento de aplicações, sendo este modelo construido acerca do conceito OBJETO;
+
+    *PARADIGMA: Modelo ou exemplo a ser seguido;
+    *PROGRAMAÇÃO: Desenvolvimento de uma aplicação;
+    *ORIENTADA: Aquilo que é regido, segue uma orientação;
+    *OBJETO: Um molde que representa uma determinada classe;
+
+    *** OBJETO---> É o que executa faz tudo acontecer, deve estar numa classe estática, é a representação de um conceito/entidade do mundo real, que pode ser física ou conceitual;
     Possui significado bem definido para determinado software;
     Para esse conceito/entiade(Objeto) deve ser inicialmente definida uma classe a partir de onde swerão instanciados objetos distintos;
 
-        EX:             Carro carro = new Carro();         //Constrói um objeto e atribui a essa variável;
+        EX:   Carro carro = new Carro();         //Constrói um objeto e atribui a essa variável;
 
     * Definição: O POO(Programação Orientada á Objeto), é um paradigma de análise, projeto de programação d sistemas de software,
-    baseado na composição e interação entre diversas unidades de software chamadas de OBJETO;
+    baseado na composição e interação entre diversas unidades de software chamadas OBJETO;
 
-    *Análise: Compreender e desenvolver forma com maior automação possível pra o projeto;
+    *Análise: Compreender e desenvolver forma com maior automação possível para o projeto;
 
-    *Projeto: Pegar necessidades do mundo real e modelá-las e criar um modelo computacional;
+    *Projeto: Pegar necessidades reais e modelá-las e criar um modelo computacional;
 
     *Programação: Desenvolver os passoas anteriores em línguagem de programação;
 
@@ -24,35 +31,35 @@ public class ResumoPOO {
 
     @ Abstração: Processo pelo qual isolamos características de um objeto como essênciais, considerando em comum os que tenham certos grupos de objetos com características essênciais iguais, para garantir o reuso;
 
-    @ Reuso: Capacidade de criar novas unidades de código a partir de outras já existentes á partir de abstração, ;
+    @ Reuso: Capacidade de criar unidades de código a partir de outras já existentes á partir de abstração, ;
 
     @ Encapsulamentos: Capacidade de esconder complexidades(Esconde comportamentos), e proteger dados aumentando a segurança desses dados,
 
 ""---->>>>> Ao programar pense apenas no PRESENTE, diminua as atribuiçoes em principais, e siga com os complementos e características, após avaliar o que já se tem, nunca tente prever o futuro;
 
-    * *** Paradigma Orientado á Objeto(POO):  tem uma representação mais realista os conceitos do mundo real com mecanismos mais avançadas;
-    * Foca na modelagem  de entidades e nas interações entre elas; Foca no "O que fazer"; Faz com  que o principal foco seja em automatizar detrminados processos;
+    * *** Paradigma Orientado á Objeto(POO): tem uma representação mais realista os conceitos do mundo real com mecanismos mais avançadas;
+    * Foca na modelagem de entidades e nas interações entre elas; Foca no "O que fazer"; Faz com  que o principal foco seja em automatizar detrminados processos;
     * Melhor COESÃO;
-    * Melhor ACOPLAMENTO -> Bons acoplamentos são flexíveis, com facilidade de modelagem;
-    * Diminuiçãodo Gap semântico(Diferença de semântica) aproxima a linguagem de máquina do DEV ;
-    * Coletor de lixo -> Libera memória automaticamente para evitar excesso de uso de memória e travamento do software ;
+    * Melhor ACOPLAMENTO---> Bons acoplamentos são flexíveis, com facilidade de modelagem;
+    * Diminuiçãodo Gap---> semântico(Diferença de semântica) aproxima a linguagem de máquina do DEV ;
+    * Coletor de lixo---> Libera memória automaticamente para evitar excesso de uso de memória e travamento do software ;
     * Em POO o foco é nas estruturas que utilizará para criar a aplicação, permitindo facilidade de solução de problemas;
 
  *** Paradigma Estruturado(PE): Utilizado pela linguagem C tem uma representação mais simplista; foca em operações(funções) e dados;
-    * Foca mais nas funçoes ou seja em dados brutos; Foca em como fazer; Muito baixo nível;
-    * Limpeza de lixo manual, aumenta chance de erro no código;
+    * Foca mais nas funçoes, ou seja em dados brutos; Foca em como fazer; Muito baixo nível;
+    * Limpeza de lixo manual, aumenta a hipótese de erro no código;
 
 ************Estruturas básicas de OO*****************
 
     *** CLASSE: É estatica serve de molde para criar objetos, unidade mínima e básica de implementação de POO, estrutura que abstrai um conjunto de OBJETOS com características similares;
-    Uma Carro define o comportamento de seus Objetos através de métodos e os estados possíveis destes através de atributos;
-    Ou seja uma CLASSE descreve os servoços providos por seus objetos e quais informações eles podem armazenar;
+    Um Carro define o comportamento dos seus Objetos através de métodos e os estados possíveis destes através de atributos;
+    Ou seja, uma CLASSE descreve os servoços providos por seus objetos e quais informações eles podem armazenar;
     Através da classe podemos modelar os dados fazendo abstração e criar objetos;
 
-    Como Criar Classes-----> São SUBSTANTIVOS do mundo real; Nomes Significativos; Contexto deve ser considerado;
+    Como Criar Classes-----> São SUBSTANTIVOS do "mundo real"; Nomes Significativos; Contexto deve ser considerado;
 
-    *** ATRIBUTOS(Próprio peculiar a alguém ou alguma coisa): é o elemento de uma Classe responsãvel por DEFINIR sua ESTRUTURA DE DADOS;
-    O conjunto destes será responsável por representar suas características e fará parte dos objetos criados a partir da classe;
+    *** ATRIBUTOS(Próprio peculiar a alguém ou algo): é o elemento de uma Classe responsãvel por DEFINIR sua ESTRUTURA DE DADOS;
+    O conjunto destes será responsável por representar as suas características e fará parte dos objetos criados a partir da classe;
     Os atributos representam de forma mais fidedigna os conceitos do mundo real;
     Dentro do pc os atributos e variáveis serão reconhecidos da mesma maneira, porém com OO os atributos serão criadas dentro das Classes;
     Já as VARIÁVEIS(Sujeito a variações, inconstanjte, instavél) serão definidos dentro dos métodos;
@@ -66,7 +73,7 @@ public class ResumoPOO {
 
     Usar: Verbos; Visibilidade; retorno; Nomes Significativos; Parâmentros se necessário; Contexto deve ser considerado;
 
-    1-Método Construtor---> Constrói Objetos:  Obrigatório uso do mesmo nome da Classe no objeto;
+    1-Método Construtor---> Constrói Objetos; Obrigatório uso do mesmo nome da Classe no objeto;
 
                               EX:        class Carro{
                                              Carro(){
